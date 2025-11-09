625da4a77cb9b5da7d91cdd2a0faafc0f5181c24
Jonathan dos Santos Borges <jonathanborges@MacBook-Air-de-Jonathan.local>
2025-11-09 01:17:51 -0300

    Exemplo do joptionpane

---
+++ JOptionPane/Media_ler.java
@@ -0,0 +1,17 @@
+package JOptionPane;
+import javax.swing.JOptionPane;
+
+public class Media_ler {
+    public static void main(String[] args) {
+
+        float nota1, nota2, calculaMedia;
+
+        nota1 = Float.parseFloat(JOptionPane.showInputDialog("Digite a primeira nota"));
+        nota2 = Float.parseFloat(JOptionPane.showInputDialog("Digite a segunda nota"));
+
+        calculaMedia = (nota1 + nota2) / 2;
+
+        JOptionPane.showMessageDialog(null, "Resultado da Média = " + calculaMedia, "Resultado",JOptionPane.WARNING_MESSAGE
+        );
+    }
+}
